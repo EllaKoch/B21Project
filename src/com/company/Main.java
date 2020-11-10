935a01f3d04f5735de0b33efc30492bd14b2a42d
EllaKoch <ms.kosya555@gmail.com>
2020-11-10 12:18:58 -0500

    added comment

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,12 +5,7 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello, git!");
-
-
-
-
-
-
+        // we are good at IntelliJ
 
 
 
